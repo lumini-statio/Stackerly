42cc99d2f51277b4f90f6f62766d1e91d4b79c73
Emi <lunaemilio2003@gmail.com>
2025-07-02 18:34:28 -0300

    new command to load csv files

---
+++ stock/management/commands/load_csv.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand
+from django.conf import settings
+import csv
+from datetime import date
+from itertools import islice
+from stock.models import CO2
+
+
+class Command(BaseCommand):
+    help = 'Load CSV data from CO2 file'
+
+    def handle(self, *args, **kwargs):
+        datafile = settings.BASE_DIR / 'stock' / 'data' / 'co2.csv'
+
+        with open(datafile, 'r') as file:
+            reader = csv.DictReader(islice(file, 40, None))
+
+            for row in reader:
+                dt = date(
+                    year=int(row['year']),
+                    month=int(row['month']),
+                    day=1
+                )
+
+                CO2.objects.get_or_create(date=dt, average=float(row['average']))
