908d60b372c109b77ebc67c49c24392c6ec2c903
Emi <lunaemilio2003@gmail.com>
2025-07-04 14:08:06 -0300

    develop on admin site

--- stock/admin.py
+++ stock/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from stock.models import *
 
 # Register your models here.
+admin.site.register(Location)
+admin.site.register(BalanceBox)
+admin.site.register(Store)
+admin.site.register(ProductState)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(Purchase)
+
+admin.site.site_title = "Admin Site"
+admin.site.site_header = "Stock Management | Admin"
+admin.site.index_title = "Admin"
