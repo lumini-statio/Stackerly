1c56fac62d1fd7dd6a563980d6094f08b20ec7af
Emi <lunaemilio2003@gmail.com>
2025-07-15 01:03:10 -0300

    changes

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-urj-(2(-3u-8a-&09_#am8$-66p_t&7&ic73x@4l3+1+*11jlc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '.onrender.com',
