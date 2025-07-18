28c287f65f45b95c6797ee731cad19e44f68dba9
Emi <lunaemilio2003@gmail.com>
2025-07-18 19:26:37 -0300

    updated settings and urls

--- app/settings.py
+++ app/settings.py
@@ -25,13 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-urj-(2(-3u-8a-&09_#am8$-66p_t&7&ic73x@4l3+1+*11jlc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    '.onrender.com',
+    'stackerly.onrender.com',
     '127.0.0.1',
     'localhost'
-    ]
+]
 
 
 # Application definition
@@ -148,7 +148,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = './stock/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
