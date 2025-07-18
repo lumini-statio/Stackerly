ee4719be75cb58f263345de5e93371841ee379bf
Emi <lunaemilio2003@gmail.com>
2025-07-18 19:54:41 -0300

    new logging

--- app/settings.py
+++ app/settings.py
@@ -145,6 +145,21 @@ USE_I18N = True
 USE_TZ = True
 
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG' if DEBUG else 'INFO',
+    },
+}
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
